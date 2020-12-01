5b954fc8a85b47a42b9c8c73a7b278d595abde8c
Andrey <andrey.savchenko@tala.co>
2020-12-01 08:37:47 +0300

    rename methods for AnimatorSet

--- app/src/main/java/ru/appngo/animationstest/activities/AnimatorSetActivity.kt
+++ app/src/main/java/ru/appngo/animationstest/activities/AnimatorSetActivity.kt
@@ -8,18 +8,21 @@ import android.view.View
 import android.view.animation.BounceInterpolator
 import android.widget.FrameLayout
 import androidx.core.content.ContextCompat
-import kotlinx.android.synthetic.main.activity_scale_from_pixel.*
+import kotlinx.android.synthetic.main.activity_animator_set.*
 import ru.appngo.animationstest.R
 
-class ScaleFromPixelActivity : Activity() {
+class AnimatorSetActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scale_from_pixel)
+        setContentView(R.layout.activity_animator_set)
         val view = addView()
-        playWithDelay(view)
+        animatorBuilderPattern(view)
     }
 
+    /*
+    Creates view 1*1 pixel.
+     */
     private fun addView(): View {
         val view = View(this).apply {
             layoutParams = FrameLayout.LayoutParams(1, 1).apply {
@@ -31,7 +34,7 @@ class ScaleFromPixelActivity : Activity() {
         return view
     }
 
-    private fun playWithDelay(view: View) {
+    private fun animatorBuilderPattern(view: View) {
         val set = AnimatorSet()
         set.duration = 3000
         set.interpolator = BounceInterpolator()
@@ -40,23 +43,23 @@ class ScaleFromPixelActivity : Activity() {
         set.start()
     }
 
-    private fun scaleView(view: View) {
+    private fun animatorSetSequentially(view: View) {
         AnimatorSet().apply {
             duration = 3000
             interpolator = BounceInterpolator()
         }.run {
-            play(ObjectAnimator.ofFloat(view, View.SCALE_X, 200f))
-                .after(1000L)
-                .after(ObjectAnimator.ofFloat(view, View.SCALE_Y, 200f))
             playSequentially(
-                ObjectAnimator.ofFloat(view, View.SCALE_X, 200f),
+                ObjectAnimator.ofFloat(view, View.SCALE_X, 200f).apply {
+                    //delay before each animation if required
+                    startDelay = 1000
+                },
                 ObjectAnimator.ofFloat(view, View.SCALE_Y, 200f)
             )
             start()
         }
     }
 
-    private fun playNumberOfAnimators(view: View) {
+    private fun animateNumberOfAnimators(view: View) {
         ObjectAnimator.ofFloat(view, View.SCALE_X, 200f).apply {
             duration = 3000
             interpolator = BounceInterpolator()
