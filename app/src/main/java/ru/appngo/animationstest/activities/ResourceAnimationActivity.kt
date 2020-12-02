bacbbf56513a563ea13c7feb631a37dc25a8d0e3
Andrey <andrey.savchenko@tala.co>
2020-12-02 12:04:24 +0300

    make ResourceAnimationActivity more stilized

--- app/src/main/java/ru/appngo/animationstest/activities/ResourceAnimationActivity.kt
+++ app/src/main/java/ru/appngo/animationstest/activities/ResourceAnimationActivity.kt
@@ -3,8 +3,7 @@ package ru.appngo.animationstest.activities
 import android.app.Activity
 import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_resource_animation.*
 import ru.appngo.animationstest.R
 
 
@@ -15,10 +14,10 @@ class ResourceAnimationActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resource_animation)
-        findViewById<Button>(R.id.test_view).setOnClickListener {
+        test_view.setOnClickListener {
             rocketAnimation.start()
         }
-        findViewById<ImageView>(R.id.human_to_android).apply {
+        human_to_android.apply {
             setBackgroundResource(R.drawable.from_human_to_android)
             rocketAnimation = background as AnimationDrawable
         }
